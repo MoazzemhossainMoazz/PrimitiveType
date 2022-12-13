aaeaa9d99469dfbb43b97031d27b97d7b5e0cf9b
Moazz <moazzemh222@gmail.com>
2022-12-13 20:23:49 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimitiveType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstName = "Moazzem";
+            var lastName = "Hossain";
+            var fullName = firstName + " " + lastName;
+            Console.WriteLine(fullName);
+            Console.ReadLine();
+        }
+    }
+}
